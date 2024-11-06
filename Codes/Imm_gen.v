0f2a284311624b4625470180251670ec69a897d2
Siyuan Jing <crazycyy233@gmail.com>
2024-11-06 15:01:03 -0500

    Update Imm_gen.v

--- Codes/Imm_gen.v
+++ Codes/Imm_gen.v
@@ -1,3 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Siyuan Jing and Haonan Wu
+// 
+// Create Date: 11/04/2024 02:58:47 PM
+// Design Name: 
+// Module Name: Imm_gen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module Imm_gen(
     input [31:0] Instruction32,
     output reg [31:0] Imm_out
@@ -9,6 +30,6 @@ module Imm_gen(
         wire imm4_1 = Instruction32[11:8];       
         wire imm11 = Instruction32[7];           
 
-        imm_out = {{20{imm12}}, imm12, imm11, imm10_5, imm4_1, 1'b0};
+        Imm_out = {{20{imm12}}, imm12, imm11, imm10_5, imm4_1, 1'b0};
     end
 endmodule
