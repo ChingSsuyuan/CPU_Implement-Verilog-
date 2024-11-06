01fcda8fb4ffa20be95b089185639f29bfc96a4b
Siyuan Jing <crazycyy233@gmail.com>
2024-11-06 14:57:14 -0500

    Update Imm_gen.v

--- Codes/Imm_gen.v
+++ Codes/Imm_gen.v
@@ -1,1 +1,14 @@
+module imm_gen(
+    input [31:0] Instruction32,
+    output reg [31:0] imm
+);
+    
+    always @(*) begin
+        wire imm12 = Instruction32[31];         
+        wire imm10_5 = Instruction32[30:25];   
+        wire imm4_1 = Instruction32[11:8];       
+        wire imm11 = Instruction32[7];           
 
+        imm = {{20{imm12}}, imm12, imm11, imm10_5, imm4_1, 1'b0};
+    end
+endmodule
