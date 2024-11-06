0294bdf83e5127eb326513a26e530934dadb3c52
HaonanWu17 <whn17@bu.edu>
2024-11-06 17:38:36 -0500

    Update Top_Module_tb.v

--- Codes/Top_Module_tb.v
+++ Codes/Top_Module_tb.v
@@ -6,29 +6,28 @@ module Top_Module_tb;
     reg clk;
     reg reset;
 
-    // 实例化 Top_Module
+
     Top_Module uut (
         .clk(clk),
         .reset(reset)
     );
 
-    // 时钟周期为10ns，时钟信号的生成
+    
     initial begin
         clk = 0;
-        forever #5 clk = ~clk;  // 每5ns翻转时钟信号
+        forever #5 clk = ~clk;  
     end
 
-    // 测试初始化
+    
     initial begin
-        // 初始复位信号
+    
         reset = 1;
-        #10;                   // 等待10ns
-        reset = 0;             // 释放复位信号
+        #10;                   
+        reset = 0;           
         
-        // 仿真时间延长
-        #200;                  // 等待200ns观察输出
+   
+        #1000;                 
 
-        // 结束仿真
         $stop;
     end
 
