2735b3521545c9acee9833130ca4e5fa7b0844ec
HaonanWu17 <whn17@bu.edu>
2024-11-06 16:04:57 -0500

    Update Top_module.v

--- Codes/Top_module.v
+++ Codes/Top_module.v
@@ -11,13 +11,13 @@ module Top_Module(
         .pc(pc)
     );
 
-    // 实例化 Instruction_Memory
+    
     Instruction_Memory IM(
         .Address(pc),
         .ReadData1(ReadData1)
     );
 
-    // 实例化 Main_Control_Unit
+   
     Main_Control_Unit MCU(
         .ReadInstruction(ReadData1),
         .Alucontrol(), 
@@ -35,19 +35,19 @@ module Top_Module(
     );
     
     Register_File RF(
-        .Instruction32(ReadData1),  // 将 ReadData1 连接到 Instruction32
+        .Instruction32(ReadData1),
         .WriteData(),
-        .WriteEnable(Regwrite),  // 将 Main_Control_Unit 的 Regwrite 连接到 WriteEnable
+        .WriteEnable(Regwrite),  
         .ReadData1(ReadData1),
         .ReadData2(ReadData2),
         .clk(clk),
         .rst(reset)
     );
     Mux mux_1(
-        .A(ReadData2),        // A 输入连接到 ReadData2
-        .B(Imm_out),          // B 输入连接到 Imm_out
-        .S(Alusrc),           // 选择信号 S 连接到 Alusrc
-        .Z(MuxOut)            // Mux 的输出
+        .A(ReadData2),        
+        .B(Imm_out),       
+        .S(Alusrc),          
+        .Z(MuxOut)            
     );
     
 endmodule
